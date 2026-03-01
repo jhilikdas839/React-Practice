4a11fe17ec1038fc4bdc9ca848eea17e1ff2f64e
jhilikdas839 <jhilikdas839@gmail.com>
2026-03-01 20:44:24 +0530

    SplineTutorial

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import React from "react";
 // import Demo from "./components/Demo";
 // import Obj from "./components/Obj";
 // import Twb from "./components/Twb";
-import Notes from "./components/Notes";
+// import Notes from "./components/Notes";
+import SplineDemo from "./components/SplineDemo";
 const App = () => {
   return (
     <div>
       {/* <Demo /> */}
       {/* <Obj /> */}
       {/* <Twb /> */}
-      <Notes />
+      {/* <Notes /> */}
+
+      <SplineDemo />
     </div>
   );
 };
---
+++ src/components/SplineDemo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+const SplineDemo = () => {
+  return (
+    <div className="Main">
+      <Spline scene="https://prod.spline.design/Rk5EkSbc4sSPsHwb/scene.splinecode" />
+    </div>
+  );
+};
+
+export default SplineDemo;
