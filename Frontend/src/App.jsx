fb4fc1d5cb3f5fe4e3bb0b5a4723f11d3872d685
jhilikdas839 <jhilikdas839@gmail.com>
2026-08-17 18:46:27 +0530

    Update React project with CRUD and API integration

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
 
+import axios from './api/AxiosConfig'
+import {useEffect} from "react"
 function App() {
+
+const getUser = async ()=>{
+  try {
+    const res = await axios.get("/Users")
+    console.log(res.data);
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+useEffect(() => {
+  getUser()
+}, [])
+
+
   return (
-    <div>App</div>
+    <div className='text-blue-700'>App</div>
   )
 }
 
